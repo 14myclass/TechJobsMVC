ee491c2fb5f741dbf87fdc64167029d7f10c8ae4
Rakiyah Faatuono <just4raki@hotmail.com>
2017-11-30 13:11:43 -0600

    added to if condition on search for all

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -21,12 +21,12 @@ namespace TechJobs.Controllers
             if (searchType.Equals("all"))
             {
                 List<Dictionary<string, string>> Jobs = JobData.FindByValue(searchTerm);
-                //ViewBag.columns = ListController.columnChoices;
+                ViewBag.columns = ListController.columnChoices;
 
                 ViewBag.jobs = Jobs;
 
-                return View("Jobs");
-
+                return View("Index");
+                
             }
 
 
